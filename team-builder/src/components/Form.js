c311214ae1e00761fd04f948cd0ebe7b4281cf39
Robert J. Mitchell <robert.judson@outlook.com>
2019-12-18 00:35:33 -0500

    corrected basic form

--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -2,26 +2,48 @@ import React from 'react';
 import { useState } from 'react';
 
 const Form = () => {
-  
+
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+
+  const handleUserNameChange = event => {
+    setUserName(event.target.value);
+  }
+  const handleEmailChange = event => {
+    setEmail(event.target.value);
+  }
+  const handleRoleChange = event => {
+    setRole(event.target.value);
+  }
+
+  const submitHandler = event => {
+    console.log('userName');
+    console.log('email');
+    console.log('role');
+    event.preventDefault();
+  };
+
   return (
+
     <div>
-      <form id='name'>
+      {console.log({ userName })}
+      {console.log({ email })}
+      {console.log({ role })}
+      <form onSubmit={event => submitHandler(event)}>
         <label>
           Full Name:
-          <input type='text'placeholder='e.g. John Doe' />
+          <input type='text'placeholder='e.g. John Doe' onChange={event => handleUserNameChange(event)} />
         </label>
-      </form>
-      <form id='email'>
         <label>
           Email: 
-          <input type='text' placeholder='e.g. john.doe@gmail.com'/>
+          <input type='text' placeholder='e.g. john.doe@gmail.com' onChange={event => handleEmailChange(event)}/>
         </label>
-      </form>
-      <form id='role'>
         <label>
           Role:
-          <input type='text' placeholder='e.g. Front-End Engineer'/>
+          <input type='text' placeholder='e.g. Front-End Engineer' onChange={event => handleRoleChange(event)} />
         </label>
+        <button>Submit</button>
       </form>
     </div>
   )
