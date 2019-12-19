0e9805e2ccd55eeec61819bd06fe1c36abaf6ba8
Robert J. Mitchell <robert.judson@outlook.com>
2019-12-18 21:42:28 -0500

    built out skeleton and table

---
+++ team-builder/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import UserTable from './components/UserTable';
+
+const App = () => {
+  return (
+    <div className='container'>
+      <h1>Team Builder</h1>
+      <div className='flex-row'>
+        <div className='flex-large'>
+          <h2>Add User</h2>
+        </div>
+        <div className='flex-large'>
+          <h2>View Users</h2>
+          <UserTable />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App;
