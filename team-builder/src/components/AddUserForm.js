cffc0a3338863d3b09959bec0345786c6fa9dcc2
Robert J. Mitchell <robert.judson@outlook.com>
2019-12-18 22:09:33 -0500

    adding to form works

--- team-builder/src/components/AddUserForm.js
+++ team-builder/src/components/AddUserForm.js
@@ -2,14 +2,30 @@ import React, { useState } from 'react';
 
 const AddUserForm = props => {
 
+  const initialFormState = { id: null, name: '', email: '', role: '' }
+  const [user, setUser] = useState(initialFormState)
+
+  const handleInputChange = event => {
+    const { name, value } = event.target
+
+    setUser({...user, [name]: value})
+  }
+
   return(
-    <form>
+    <form
+      onSubmit={event => {
+        event.preventDefault()
+        if (!user.name || !user.email || !user.role) return
+        props.addUser(user)
+        setUser(initialFormState)
+      }}
+    >
       <label>Name</label>
-      <input type='text' name='name' value='' />
+      <input type='text' name='name' value={user.name} onChange={handleInputChange} />
       <label>Email</label>
-      <input type='text' name='email' value='' />
+      <input type='text' name='email' value={user.email} onChange={handleInputChange} />
       <label>Role</label>
-      <input type='text' name='role' value='' />
+      <input type='text' name='role' value={user.role} onChange={handleInputChange} />
       <button>Add New Team Member</button> 
     </form>
   )
